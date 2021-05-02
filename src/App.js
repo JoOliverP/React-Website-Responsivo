9051ec22929b3a1e1792b1670200df211e7b2c2b
Jó Oliveira <joypinheiro63@gmail.com>
2021-05-02 13:53:02 -0300

    React Website Finalizado

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SingUp from './components/pages/SingUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+        <Route path='/' exact component=
+        {Home} />
+
+        <Route path='/services' component=
+        {Services} />
+
+        <Route path='/products' component=
+        {Products} />
+         <Route path='/sign-up' component=
+        {SingUp} />
+
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
